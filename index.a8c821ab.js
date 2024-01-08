01edebae89d34f39d624541c0d2a733383c6c5df
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2024-01-08 16:09:56 +0200

    make build

---
+++ index.a8c821ab.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");n.className="list",e.prepend(n),n.append(n.nextSibling)}),e.addEventListener("click",function(e){var n=e.target.parentNode.querySelector("ul");n&&(n.hidden=!n.hidden)});//# sourceMappingURL=index.a8c821ab.js.map
+
+//# sourceMappingURL=index.a8c821ab.js.map
